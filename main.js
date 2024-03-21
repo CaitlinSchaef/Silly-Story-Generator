bb0aeb47f81106a35b7edc20b8eac8589930aeb3
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-03-21 13:37:33 -0400

    updates

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-Javascript file
+//These are the complete variable and function definitions
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+//These are the raw string texts
+It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+
+Willy the Goblin
+Big Daddy
+Father Christmas
+
+the soup kitchen
+Disneyland
+the White House
+
+spontaneously combusted
+melted into a puddle on the sidewalk
+turned into a slug and crawled away
+
+//These are the event listener and partial function definitions
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
+
+//adjust as necessary to insert java into html
