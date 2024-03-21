3a64d143ba00dae6d2ba2e682db48da46963b8aa
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-03-21 13:48:17 -0400

    updated java

--- main.js
+++ main.js
@@ -8,20 +8,13 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-//These are the raw string texts
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
-Willy the Goblin
-Big Daddy
-Father Christmas
-
-the soup kitchen
-Disneyland
-the White House
-
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+//insterting provided raw string into text, added them as constants 
+{
+const $storyText = "It was 94 fahrenheit outside, so :$insertX: went for a walk. When they got to :$insertY:, they stared in horror for a few moments, then :$insertZ:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+const $insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
+const $insertY = ["the soup kitchen", "Disneyland", "the White House"]
+const $insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
+}
 
 //These are the event listener and partial function definitions
 randomize.addEventListener('click', result);
