8496b9760642abc256b896ec24c371387eac4830
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-03-21 13:58:28 -0400

    updated java

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-//These are the complete variable and function definitions
+//These are the complete variable and function definitions, given, do not edit
+
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -8,7 +9,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-//insterting provided raw string into text, added them as constants 
+//insterting provided raw string into text, added them as constants with arrays
 {
 const $storyText = "It was 94 fahrenheit outside, so :$insertX: went for a walk. When they got to :$insertY:, they stared in horror for a few moments, then :$insertZ:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 const $insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
@@ -19,11 +20,14 @@ const $insertZ = ["spontaneously combusted", "melted into a puddle on the sidewa
 //These are the event listener and partial function definitions
 randomize.addEventListener('click', result);
 
+//Make the functions below here, they provided function result frame, if custom name and if uk
+//First function is to generate new story, is this an auto wipe?
 function result() {
+  let newStory = $storyText
 
   if(customName.value !== '') {
     const name = customName.value;
-
+new story =
   }
 
   if(document.getElementById("uk").checked) {
