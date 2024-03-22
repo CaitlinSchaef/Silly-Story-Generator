adc3abe40bebcf633ad26d70bd94fc03666d62b7
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-03-22 12:38:50 -0400

    colon

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ newStory = newStory.replaceAll('Bob', name);
     const weight = '${Math.round(300/14)} stone';
     const temperature =  '${Math.round(94− 32) × 5/9)} centigrade';
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
-    newStory = newStory.replaceAll('300 pounds', weight)
+    newStory = newStory.replaceAll('300 pounds', weight);
   }
 // the above was broken and honestly i just added random quotes until it no longer erred
   
